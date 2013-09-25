228fdef20164a9cc00139394a149f97c50b1faa5
Tomas <tomas@aparicio.me>
2013-09-26 00:21:21 +0200

    Upgrade sass gem to 3.2.10. Checking new version on new bash session

--- bash.sh
+++ bash.sh
@@ -5,8 +5,56 @@
 
 env_path=$(cd "$( dirname "${BASH_SOURCE[0]}")" && pwd)
 version=$(head -1 "$env_path/VERSION" | awk '{print $1}')
+version_check_url='https://raw.github.com/frontstack/stack/master/VERSION'
+version_file='/tmp/frontstack-version'
+output='/tmp/frontstack.log'
+
+exists() {
+  type $1 >/dev/null 2>&1;
+  if [ $? -eq 0 ]; then
+    echo 1
+  else
+    echo 0
+  fi
+}
+
+clean_files() {
+  rm -rf $output
+  rm -rf $lastest_version_file
+  rm -rf $version_file
+}
+
+get_version() {
+  if [ ! -f "$1" ]; then
+    echo 0
+  else
+    head -1 "$1" | awk '{print $1}'
+  fi
+}
 
 echo "Welcome to FrontStack $version"
+
+if [ `exists wget` -eq 1 ]; then 
+  wget $version_check_url -O $version_file > $output 2>&1
+  if [ $? -eq 0 ]; then
+    latest_version=`get_version $version_file` 
+    if [ $latest_version != $version ]; then
+      
+      cat <<EOF
+
+New FrontStack version available:'
+* Local: $version"
+* Latest: $latest_version"
+
+To update your environment, simply run:
+$ $env_path/scripts/update.sh
+
+EOF
+
+    fi
+  fi
+  clean_files
+fi
 
 . "${env_path}/scripts/setenv.sh"
 
