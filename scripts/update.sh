588708fb612e41d681aa2658d65b7dc9818a60f1
Tomas Aparicio <tomas@aparicio.me>
2014-01-14 10:42:25 +0100

    feat: update.sh script refactor

--- scripts/update.sh
+++ scripts/update.sh
@@ -23,8 +23,10 @@ clean_files() {
 check_exit() {
   if [ $? -ne 0 ]; then
     clean_files
-    echo $1
-    exit 1
+    for msg in $*; do 
+     echo $msg 
+   done
+   exit 1
   fi
 }
 
@@ -36,11 +38,25 @@ get_version() {
   fi
 }
 
+is_url() {
+  protocol="$(echo $1 | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+  if [ -z $protocol ]; then
+    echo 1
+  else 
+    echo 0
+  fi
+}
+
 get_download_file() {
   if [ ! -f "$1" ]; then
     echo 0
   else
-    head -1 "$1" | awk '{print $2}'
+    local file=`head -1 "$1" | awk '{print $2}'`
+    if [ `is_url "$file"` -eq 0 ]; then
+      echo $file
+    else
+      echo "${download_url}/${file}/download" 
+    fi    
   fi
 }
 
@@ -73,10 +89,6 @@ download_status() {
 
 clean_files
 
-wget http://yahoo.com -O /tmp/test.log > $output 2>&1
-check_exit "No Internet HTTP connectivity. Check if you are behind a proxy. See /tmp/test.log"
-rm -f /tmp/test.log
-
 version=`get_version $version_file` 
 if [ $version == '0' ]; then 
   echo 'Warning: cannot check the local FrontStack version'
@@ -86,13 +98,13 @@ if [ $version == '0' ]; then
   fi
 fi
 
-wget $check_url -O $lastest_version_file >> $output 2>&1
-check_exit "Cannot check the latest version. See $output"
+wget --no-check-certificate $check_url -O $lastest_version_file >> $output 2>&1
+check_exit "Cannot check the latest version, are you behind a web proxy?" "Remote version manifest: $check_url" "Log file: $output"
 latest_version=`get_version $lastest_version_file`
 [ $latest_version == '0' ] && echo "Latest version file don't exists. Check the Internet connectivity" && exit 1
 
 if [ $latest_version == $version ]; then
-  echo 'Your FrontStack environment is up to date' && exit 0
+  echo 'FrontStack is up to date' && exit 0
 fi 
 
 echo 'New FrontStack version is available'
@@ -118,7 +130,7 @@ echo 'Before continue, be sure you kill all the FrontStack running processes...'
 read -p 'Yes, I done, continue with the update... ' 
 
 echo
-`wget -F "$download_url/$(get_download_file $lastest_version_file)/download" -O $download_dir/frontstack-latest.tar.gz > $output 2>&1 && echo $? > $download_status || echo $? > $download_status` &
+`wget --no-check-certificate -F "$(get_download_file $lastest_version_file)" -O $download_dir/frontstack-latest.tar.gz > $output 2>&1 && echo $? > $download_status || echo $? > $download_status` &
 download_status $output $download_status
 check_exit "Error while trying to download FrontStack. See $output"
 
